bc400a79e8126ffab34a9621f41f31ca9b4c74e7
Stuart Moore <Stuart@MacBook-Pro.local>
2012-09-29 09:18:18 +0200

    Initial Commit

---
+++ Netcaster/NCAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  NCAppDelegate.h
+//  Netcaster
+//
+//  Created by Stuart Moore on 9/29/12.
+//  Copyright (c) 2012 Stuart Moore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
